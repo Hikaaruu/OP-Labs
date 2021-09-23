a607442254c5e0a2c015c9a46edaa77a77cc1768
Hikaaruu <90321439+Hikaaruu@users.noreply.github.com>
2021-09-23 10:25:00 +0300

    Add files via upload

---
+++ Lab№1 on C#/Lab№1 OP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_1_OP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kat1, gip, res;
+            Console.WriteLine("Введите значение гипотенузы");
+            gip = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение первого катета");
+            kat1 = Convert.ToDouble(Console.ReadLine());
+            res = Math.Sqrt(Math.Pow(gip, 2) - Math.Pow(kat1, 2));
+            Console.WriteLine("Значение второго катета:" + res);
+
+        }
+    }
+}
---
+++ Lab№1 on python/Lab№1 on python/Lab_1_on_python.py
@@ -0,0 +1,5 @@
+import math
+gip=input("Введите значение гипотенузы ")
+kat1=input("Введите значение первого катета ")
+res=math.sqrt(math.pow(float(gip),2)-math.pow(float(kat1),2))
+print("Значение второго катета: ", res)
