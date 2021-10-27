cfa57faf8e62c08e4bec6b898c197a48df80fb5b
Hikaaruu <90321439+Hikaaruu@users.noreply.github.com>
2021-10-27 20:52:39 +0300

    Add files via upload

---
+++ Lab №3 on CS/Лабораторна робота №3 CS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Лабораторна_робота__3_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x0, x, nextx;
+            Console.WriteLine("Enter a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            if (a <= 1) x0 = Math.Min(2 * a, 0.95);
+            else if ((1 < a) && (a < 25)) x0 = a / 5;
+            else x0 = a / 25;
+            x = x0;
+            nextx = ( ( 4 * x ) / 5 ) + ( a / ( 5 * ( Math.Pow(x, 4) ) ) );
+            while (Math.Abs(x - nextx) >= 0.0001) 
+                {
+                    x = nextx;
+                    nextx = ((4 * x) / 5) + (a / (5 * (Math.Pow(x, 4))));
+                }
+            Console.WriteLine(nextx);
+            
+
+
+        }
+    }
+}
