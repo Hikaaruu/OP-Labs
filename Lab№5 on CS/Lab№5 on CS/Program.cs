438e6b338955828cf5d9c601cc156c8a5af73875
Hikaaruu <90321439+Hikaaruu@users.noreply.github.com>
2021-11-04 10:50:14 +0200

    Add files via upload

--- Lab№5 on CS/Lab№5 on CS/Program.cs
+++ Lab№5 on CS/Lab№5 on CS/Program.cs
@@ -6,22 +6,42 @@ namespace Lab_5_on_CS
     {
         static void Main(string[] args)
         {
-            long a, b;
+            double a, b;
+            long a1, b1;
+            long sum, n;
+            
             Console.WriteLine("Enter a");
-            a = Convert.ToInt64(Console.ReadLine());
+            a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter b");
-            b = Convert.ToInt64(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine();
-            for (long i =a; i<=b;i++)
+            if (a < 1) { 
+             a1 = 1;
+            }
+            else  if (a % 1 != 0) { 
+            a1 = Convert.ToInt32(a - (a % 1)+1); 
+             } 
+            else { a1 = Convert.ToInt32(a); }
+            
+            b1 = Convert.ToInt32(b - (b % 1));
+
+            for (long i=a1; i<=b1;i++)
             {
+                sum = 0;
+                n = 0;
                 Console.WriteLine("divisors of "+Convert.ToString(i));
                 for (long j = 1; j<=i; j++)
                 {
                     if (i % j == 0)
                     {
                         Console.Write(Convert.ToString(j)+" ");
+                        n++;
+                        sum += j;
                     } 
                 }
+                Console.WriteLine();
+                Console.WriteLine("number of divisors = "+n);
+                Console.WriteLine("their sum = " + sum);
                 Console.WriteLine();
                 Console.WriteLine();
 
--- Lab№5 on Python/Lab№5 on Python/Lab_5_on_Python.py
+++ Lab№5 on Python/Lab№5 on Python/Lab_5_on_Python.py
@@ -1,21 +1,31 @@
-
 import math
-a = int(input("Enter a "))
-b = int(input("Enter b "))
+
+a = float(input("Enter a "))
+b = float(input("Enter b "))
 print()
-i = a
-while i<=b:
-    print("divisors of ",i)
+if a < 1:
+    a1 = 1
+elif a % 1 != 0:
+    a1 = a - (a % 1) + 1
+else:
+    a1 = a
+
+b1 = b - (b % 1)
+i = a1
+
+while i <= b1:
+    sum = 0
+    n = 0
+    print("divisors of ", i)
     j = 1
-    while j<=i:
-        if i % j == 0 : print(j," ")
-        j=j+1
-    i = i+1
+    while j <= i:
+        if (i % j) == 0:
+            print(str(j) + " ")
+            n = n + 1
+            sum += j
+        j = j + 1
+    i = i + 1
+    print("number of divisors = " + str(n))
+    print("their sum = " + str(sum))
     print()
     print()
-
-
-
-
-
-           
